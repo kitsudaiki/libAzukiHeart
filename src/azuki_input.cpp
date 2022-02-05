9c903065c93c5ae20aa7895386a5959a51152e70
Tobias Anker <tobias.anker@kitsunemimi.moe>
2022-02-05 20:34:37 +0100

    add code from old libKitsunemimiHanamiPredefinitions

---
+++ src/azuki_input.cpp
@@ -0,0 +1,78 @@
+/**
+ * @file        azuki_input.cpp
+ *
+ * @author      Tobias Anker <tobias.anker@kitsunemimi.moe>
+ *
+ * @copyright   Apache License Version 2.0
+ *
+ *      Copyright 2022 Tobias Anker
+ *
+ *      Licensed under the Apache License, Version 2.0 (the "License");
+ *      you may not use this file except in compliance with the License.
+ *      You may obtain a copy of the License at
+ *
+ *          http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *      Unless required by applicable law or agreed to in writing, software
+ *      distributed under the License is distributed on an "AS IS" BASIS,
+ *      WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *      See the License for the specific language governing permissions and
+ *      limitations under the License.
+ */
+
+#include <libAzukiHeart/azuki_input.h>
+#include <get_thread_mapping.h>
+#include <bind_thread_to_core.h>
+
+#include <libKitsunemimiSakuraLang/sakura_lang_interface.h>
+
+#include <libKitsunemimiHanamiEndpoints/endpoint.h>
+
+namespace Azuki
+{
+
+/**
+ * @brief HanamiMessaging::initAzukiBlossoms
+ * @return
+ */
+bool
+initAzukiBlossoms()
+{
+    // init predefined blossoms
+    Kitsunemimi::Sakura::SakuraLangInterface* interface =
+            Kitsunemimi::Sakura::SakuraLangInterface::getInstance();
+    Kitsunemimi::Hanami::Endpoint* endpoints = Kitsunemimi::Hanami::Endpoint::getInstance();
+    const std::string group = "-";
+
+    if(interface->addBlossom(group, "get_thread_mapping", new GetThreadMapping()) == false) {
+        return false;
+    }
+
+    // add new endpoints
+    if(endpoints->addEndpoint("v1/get_thread_mapping",
+                              Kitsunemimi::Hanami::GET_TYPE,
+                              Kitsunemimi::Hanami::BLOSSOM_TYPE,
+                              "-",
+                              "get_thread_mapping") == false)
+    {
+        return false;
+    }
+
+    if(interface->addBlossom(group, "bind_thread_to_core", new BindThreadToCore()) == false) {
+        return false;
+    }
+
+    // add new endpoints
+    if(endpoints->addEndpoint("v1/bind_thread_to_core",
+                              Kitsunemimi::Hanami::POST_TYPE,
+                              Kitsunemimi::Hanami::BLOSSOM_TYPE,
+                              "-",
+                              "bind_thread_to_core") == false)
+    {
+        return false;
+    }
+
+    return true;
+}
+
+}
